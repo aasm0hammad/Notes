4036845833129984b8a89a765f5787614c58aac9
AASM0 <aas006900@gmail.com>
2025-02-21 18:17:11 +0530

    create Database

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,8 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DbHelper{
 ///private Constructor
   DbHelper._();
 
   static DbHelper getInstance()=> DbHelper._();
+
+
+  Database? _db;
+
+Future<Database> getDB()async{
+  _db ??= await openDB();
+  return _db!;
+
+}
+
+  Future<Database> openDB() async{
+
+    Directory appDocDir= await getApplicationDocumentsDirectory();
+
+    String dbPath= join(appDocDir.path,"noteBD.db");
+
+    return await openDatabase(dbPath,onCreate: (db,version){
+      ///create tables
+      
+      db.execute("create table notes ( nId Integer primary key auto increment , nTitle text, NDesc text, nCreatedAt text   )");
+
+    });
+  }
+
+  void insertNote({ required String title, required String desc})async{
+
+  var db = await getDB();
+
+
+  }
 
 
 }
