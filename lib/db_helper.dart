879e1e536b0d83d04456f75e4b5c6d3e9bd12da2
AASM0 <aas006900@gmail.com>
2025-02-21 21:02:50 +0530

    add Notes And fetch all notes

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,45 +1,60 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
+
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DbHelper{
-///private Constructor
+class DbHelper {
+  ///private Constructor
   DbHelper._();
 
-  static DbHelper getInstance()=> DbHelper._();
+  static DbHelper getInstance() => DbHelper._();
 
+  static const String TABLE_NOTE = "notes";
+  static const String COLUMN_NOTE_ID = "nID";
+  static const String COLUMN_NOTE_TITLE = "nTitle";
+  static const String COLUMN_NOTE_DESC = "nDesc";
+  static const String COLUMN_NOTE_CREATED_AT = "nCreatedAT";
 
   Database? _db;
 
-Future<Database> getDB()async{
-  _db ??= await openDB();
-  return _db!;
+  Future<Database> getDB() async {
+    _db ??= await openDB();
+    return _db!;
+  }
 
-}
+  Future<Database> openDB() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
 
-  Future<Database> openDB() async{
+    String dbPath = join(appDocDir.path, "noteBD.db");
 
-    Directory appDocDir= await getApplicationDocumentsDirectory();
-
-    String dbPath= join(appDocDir.path,"noteBD.db");
-
-    return await openDatabase(dbPath,onCreate: (db,version){
+    return await openDatabase(dbPath,version: 1, onCreate: (db, version) {
       ///create tables
-      
-      db.execute("create table notes ( nId Integer primary key auto increment , nTitle text, NDesc text, nCreatedAt text   )");
 
+      db.execute(
+          "create table $TABLE_NOTE ( $COLUMN_NOTE_ID Integer primary key autoincrement , $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text, $COLUMN_NOTE_CREATED_AT text   )");
     });
   }
 
-  void insertNote({ required String title, required String desc})async{
+  Future<bool> addNote({required String title, required String desc}) async {
+    Database db = await getDB();
 
-  var db = await getDB();
-
-
+    int rowEffected = await db.insert(TABLE_NOTE, {
+      COLUMN_NOTE_TITLE: title,
+      COLUMN_NOTE_DESC: desc,
+      COLUMN_NOTE_CREATED_AT: DateTime.now().microsecondsSinceEpoch.toString(),
+    });
+    return rowEffected>0;
   }
 
+  Future<List<Map<String,dynamic>>> fetchAllNotes() async{
+
+    var db= await getDB();
+
+    List<Map<String,dynamic>> mNotes= await db.query(TABLE_NOTE);
+    return mNotes;
+
+  }
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/db_helper.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DbHelper? mDb;
+  List<Map<String,dynamic>> mData =[];
+
+  @override
+  void initState() {
+    super.initState();
+    mDb= DbHelper.getInstance();
+    getAllNotes();
+
+  }
+  void getAllNotes()async{
+
+    mData=await mDb!.fetchAllNotes();
+    setState(() {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
- return Scaffold(
-   body: Text("data"),
-   
- );
+    return Scaffold(
+      body: mData.isNotEmpty ? ListView.builder(
+          itemCount: mData.length,
+          itemBuilder: (_,index){
+            return ListTile(
+              title: Text(mData[index][DbHelper.COLUMN_NOTE_TITLE]),
+              subtitle: Text(mData[index][DbHelper.COLUMN_NOTE_DESC]),
+            );
+
+      }): Center(child: Text("No Notes?"),),
+      floatingActionButton: FloatingActionButton(onPressed: ()async {
+        bool check =await mDb!.addNote(title: "Today", desc: "Done");
+        if(check){
+          print("note add");
+          getAllNotes();
+        }
+
+      },child: Icon(Icons.add),),
+    );
   }
 }
